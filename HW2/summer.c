dc7adb1ac8c36bdcf63a37d9835213adb137e136
Timothy Goon <t.kgoonx@gmail.com>
2021-02-15 17:12:10 -0500

    Possible solution for problem 1 complete

--- HW2/summer.c
+++ HW2/summer.c
@@ -34,19 +34,29 @@ pthread_cond_t soloist_cond = PTHREAD_COND_INITIALIZER;
 
 void* dancer(void* arg){
   int position = 0;
+  int ret = 0;
 
-  printf("Entering Dancer\n");
+  // printf("Entering Dancer\n");
+  // ret = pthread_mutex_lock(&dancer_lock);
+  // assert(ret == 0);
+  // ret = pthread_cond_wait(&dancer_cond, &dancer_lock);
+  // assert(ret == 0);
+  // ret = pthread_mutex_unlock(&dancer_lock);
+  // assert(ret == 0);
 
   while(1){
 
     while(1){
-      pthread_mutex_lock(&dancer_lock);
+      // printf("Entering Dancer lock loop\n");
+      ret = pthread_mutex_lock(&dancer_lock);
+      assert(ret == 0);
       if(num_dancing == 0){
         // No dancers dancing, try to get onstage
-        int ret = pthread_mutex_trylock(&stage_lock);
+        ret = pthread_mutex_trylock(&stage_lock);
         if(ret == EBUSY){
           // A different type of performance is going on, go to sleep
           pthread_cond_wait(&dancer_cond, &dancer_lock);
+          pthread_mutex_unlock(&dancer_lock);
           // Go back to start of loop and try to get on stage again
           continue;
         }
@@ -59,11 +69,12 @@ void* dancer(void* arg){
         pthread_cond_signal(&dancer_cond);
         break;
 
-      } else if(num_dancing <= MAX_DANCERS){
+      } else if(num_dancing < MAX_DANCERS){
         // Another dancer is already onstage, go join
         num_dancing ++;
         position = num_dancing;
         pthread_mutex_unlock(&dancer_lock);
+        pthread_cond_signal(&dancer_cond);
         break;
 
       } else{
@@ -74,15 +85,16 @@ void* dancer(void* arg){
     }
 
     int time = (rand() % 5) + 1;
-    printf("Flamenco Dancer ID %ld will perform in stage position %d for %d seconds.\n",
-      pthread_self(), position, time);
+    int id = (pthread_self()%NUM_DANCERS)+1;
+    printf("Flamenco Dancer ID %d will perform in stage position %d for %d seconds.\n",
+      id, position, time);
 
 
     // Perform for [1, 5] seconds
     sleep(time);
 
-    printf("Flamenco Dancer ID %ld finished performing and will leave stage position %d.\n",
-      pthread_self(), position);
+    printf("Flamenco Dancer ID %d finished performing and will leave stage position %d.\n",
+      id, position);
 
     pthread_mutex_lock(&dancer_lock);
     num_dancing --;
@@ -114,18 +126,31 @@ void* dancer(void* arg){
 
 void* juggler(void* arg){
   int position = 0;
+  int ret = 0;
 
-  printf("Entering juggler\n");
+  // ret = pthread_mutex_lock(&juggler_lock);
+  // assert(ret == 0);
+  // ret = pthread_cond_wait(&juggler_cond, &juggler_lock);
+  // assert(ret == 0);
+  // ret = pthread_mutex_unlock(&juggler_lock);
+  // assert(ret == 0);
+
+  // printf("Entering juggler\n");
 
   while(1){
     while(1){
-      pthread_mutex_lock(&juggler_lock);
+      // printf("Entering juggler lock loop\n");
+      ret = pthread_mutex_lock(&juggler_lock);
+      assert(ret == 0);
       if(num_juggling == 0){
         // No jugglers juggling, try to get onstage
-        int ret = pthread_mutex_trylock(&stage_lock);
+        ret = pthread_mutex_trylock(&stage_lock);
         if(ret == EBUSY){
           // A different type of performance is going on, go to sleep
-          pthread_cond_wait(&juggler_cond, &juggler_lock);
+          ret = pthread_cond_wait(&juggler_cond, &juggler_lock);
+          assert(ret == 0);
+          ret = pthread_mutex_unlock(&juggler_lock);
+          assert(ret == 0);
           // Go back to start of loop and try to get on stage again
           continue;
         }
@@ -133,58 +158,78 @@ void* juggler(void* arg){
         // Wake another juggler then go perform
         num_juggling ++;
         position = num_juggling;
-        pthread_mutex_unlock(&juggler_lock);
+        ret = pthread_mutex_unlock(&juggler_lock);
+        assert(ret == 0);
 
-        pthread_cond_signal(&juggler_cond);
+        ret = pthread_cond_signal(&juggler_cond);
+        assert(ret == 0);
         break;
 
-      } else if(num_juggling <= MAX_JUGGLERS){
+      } else if(num_juggling < MAX_JUGGLERS){
         // Another juggler is already onstage, go join
         num_juggling ++;
         position = num_juggling;
-        pthread_mutex_unlock(&juggler_lock);
+        ret = pthread_mutex_unlock(&juggler_lock);
+        assert(ret == 0);
+        ret = pthread_cond_signal(&juggler_cond);
+        assert(ret == 0);
         break;
 
       } else{
         // The max number of jugglers are onstage, sleep
-        pthread_cond_wait(&juggler_cond, &juggler_lock);
-        pthread_mutex_unlock(&juggler_lock);
+        ret = pthread_cond_wait(&juggler_cond, &juggler_lock);
+        assert(ret == 0);
+        ret = pthread_mutex_unlock(&juggler_lock);
+        assert(ret == 0);
       }
     }
 
     int time = (rand() % 5) + 1;
-    printf("Juggler ID %ld will perform in stage position %d for %d seconds.\n",
-      pthread_self(), position, time);
+    int id = (pthread_self()%NUM_JUGGLERS)+1;
+    printf("Juggler ID %d will perform in stage position %d for %d seconds.\n",
+      id, position, time);
 
 
     // Perform for [1, 5] seconds
     sleep(time);
 
-    printf("Juggler ID %ld finished performing and will leave stage position %d.\n",
-      pthread_self(), position);
+    printf("Juggler ID %d finished performing and will leave stage position %d.\n",
+      id, position);
 
-    pthread_mutex_lock(&juggler_lock);
+    ret = pthread_mutex_lock(&juggler_lock);
+    assert(ret == 0);
     num_juggling --;
     // Sleep if you are not the last juggler on stage
     if(num_juggling > 0){
-      pthread_cond_wait(&juggler_cond, &juggler_lock);
-      pthread_mutex_unlock(&juggler_lock);
+      ret = pthread_cond_wait(&juggler_cond, &juggler_lock);
+      assert(ret == 0);
+      ret = pthread_mutex_unlock(&juggler_lock);
+      assert(ret == 0);
     } else{
-      pthread_mutex_unlock(&juggler_lock);
-      pthread_mutex_unlock(&stage_lock);
+      ret = pthread_mutex_unlock(&juggler_lock);
+      assert(ret == 0);
+      ret = pthread_mutex_unlock(&stage_lock);
+      assert(ret == 0);
       // The last juggler picks the next performer
       // Randomly pick a dancer or soloist to go next
       // 2 to 1 in favor of dancers
       int pick = rand() % 3;
-      if(pick > 0)
-        pthread_cond_signal(&dancer_cond);
-      else
-        pthread_cond_signal(&soloist_cond);
+      if(pick > 0){
+        ret = pthread_cond_signal(&dancer_cond);
+        assert(ret == 0);
+      }
+      else{
+        ret = pthread_cond_signal(&soloist_cond);
+        assert(ret == 0);
+      }
 
       // Go to sleep
-      pthread_mutex_lock(&juggler_lock);
-      pthread_cond_wait(&juggler_cond, &juggler_lock);
-      pthread_mutex_unlock(&juggler_lock);
+      ret = pthread_mutex_lock(&juggler_lock);
+      assert(ret == 0);
+      ret = pthread_cond_wait(&juggler_cond, &juggler_lock);
+      assert(ret == 0);
+      ret = pthread_mutex_unlock(&juggler_lock);
+      assert(ret == 0);
     }
   }
 
@@ -192,50 +237,73 @@ void* juggler(void* arg){
 }
 
 void* soloist(void* arg){
-  printf("Entering soloist\n");
+  // printf("Entering soloist\n");
+  int ret = 0;
+
+  // ret = pthread_mutex_lock(&soloist_lock);
+  // assert(ret == 0);
+  // ret = pthread_cond_wait(&soloist_cond, &soloist_lock);
+  // assert(ret == 0);
+  // ret = pthread_mutex_unlock(&soloist_lock);
+  // assert(ret == 0);
 
   while(1){
     while(1){
-
-      pthread_mutex_lock(&soloist_lock);
+      // printf("Entering soloist lock loop\n");
+      ret = pthread_mutex_lock(&soloist_lock);
+      assert(ret == 0);
       // try to get onstage
-      int ret = pthread_mutex_trylock(&stage_lock);
+      ret = pthread_mutex_trylock(&stage_lock);
       if(ret == EBUSY){
         // A different type of performance is going on, go to sleep
-        pthread_cond_wait(&soloist_cond, &juggler_lock);
+        ret = pthread_cond_wait(&soloist_cond, &soloist_lock);
+        assert(ret == 0);
       } else{
-        pthread_mutex_unlock(&soloist_lock);
+        ret = pthread_mutex_unlock(&soloist_lock);
+        assert(ret == 0);
         break;
       }
 
-      pthread_mutex_unlock(&soloist_lock);
+      ret = pthread_mutex_unlock(&soloist_lock);
+      assert(ret == 0);
     }
 
     int time = (rand() % 5) + 1;
-    printf("Soloist ID %ld will perform in all stage positions for %d seconds.\n",
-      pthread_self(), time);
+    int id = (pthread_self()%NUM_SOLOIST)+1;
+    printf("Soloist ID %d will perform in all stage positions for %d seconds.\n",
+      id, time);
 
 
     // Perform for [1, 5] seconds
     sleep(time);
 
-    printf("Soloist ID %ld finished performing and will leave the stage.\n",
-      pthread_self());
+    printf("Soloist ID %d finished performing and will leave the stage.\n",
+      id);
 
-    pthread_mutex_unlock(&stage_lock);
+    ret = pthread_mutex_unlock(&stage_lock);
+    assert(ret == 0);
     // Picks the next performer
     // Randomly pick a dancer or juggler to go next
     // 2 to 1 in favor of dancers
     int pick = rand() % 3;
-    if(pick > 0)
-      pthread_cond_signal(&dancer_cond);
-    else
-      pthread_cond_signal(&juggler_cond);
+    if(pick > 0){
+      // printf("Soloist picking dancer.\n");
+      ret = pthread_cond_signal(&dancer_cond);
+      assert(ret == 0);
+    }
+    else{
+      ret = pthread_cond_signal(&juggler_cond);
+      assert(ret == 0);
+      // printf("Soloist picking juggler.\n");
+    }
 
     // Go to sleep
-    pthread_mutex_lock(&soloist_lock);
-    pthread_cond_wait(&soloist_cond, &soloist_lock);
-    pthread_mutex_unlock(&soloist_lock);
+    ret = pthread_mutex_lock(&soloist_lock);
+    assert(ret == 0);
+    ret = pthread_cond_wait(&soloist_cond, &soloist_lock);
+    assert(ret == 0);
+    ret = pthread_mutex_unlock(&soloist_lock);
+    assert(ret == 0);
 
   }
 
@@ -271,6 +339,7 @@ int main(void){
     assert(ret == 0);
     jugglers[i] = &thread;
   }
+
 
   // Create the soloist threads
   for(int i=0; i<NUM_SOLOIST; i++){

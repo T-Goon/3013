861aa5fe7a24ccf31317da3323fd89d58dcb7e44
Timothy Goon <t.kgoonx@gmail.com>
2021-02-18 00:31:01 -0500

    Outlined the worker thread and figured out solution.

--- HW2/fedoops.c
+++ HW2/fedoops.c
@@ -5,14 +5,14 @@
 
 #define NUM_PACKAGES 20
 #define MAX_INSTR 4
+#define TEAM_SIZE 10
 
 // Instructions
 enum instr{
   WEIGH,
   BARCODE,
   XRAY,
-  JOSTLE,
-  DONE
+  JOSTLE
 };
 
 // worker types
@@ -27,12 +27,14 @@ enum color{
 struct instrNode{
   enum  instr instruction;
   struct instrNode* next;
+  struct instrNode* prev;
 };
 
 // The package
 struct package{
   int id;
-  struct instrNode* instrList;
+  struct instrNode* instrListHead;
+  struct instrNode* instrListTail;
 };
 
 // Linked list node for the packages
@@ -64,11 +66,46 @@ void doWork(){
 
 }
 
+void get_station_lock(enum instr instruction){
+  switch (instruction) {
+    case WEIGH:
+      sem_wait(&scale_lock);
+      break;
+    case BARCODE:
+      sem_wait(&barcoder_lock);
+      break;
+    case XRAY:
+      sem_wait(&xray_lock);
+      break;
+    case JOSTLE:
+      sem_wait(&shaker_lock);
+      break;
+  }
+}
+
 void* worker(void* args){
   int id = ((struct workerArgs*)args)->id;
   enum color type = ((struct workerArgs*)args)->c;
 
   while(1){
+    // Try to get the team lock
+    while(1){
+      switch (type) {
+        case RED:
+          sem_wait(&red_sem);
+          break;
+        case BLUE:
+          sem_wait(&blue_sem);
+          break;
+        case GREEN:
+          sem_wait(&green_sem);
+          break;
+        case YELLOW:
+          sem_wait(&yellow_sem);
+          break;
+      }
+    }
+
     // Lock queue
     sem_wait(&pkg_q_lock);
     if(head == NULL){
@@ -78,17 +115,33 @@ void* worker(void* args){
       break;
     }
 
-    // Get package from queue
-    struct packge* my_package = head;
+    // Get node from queue
+    struct pkgQueueNode* pkg_node = head;
     head = head->next;
 
     // Unlock queue
     sem_post(&pkg_q_lock);
 
-    // Get next instruction
-    struct instrNode* curInstr = my_package->instrList;
+    // unpack the package
+    struct package* my_package = pkg_node->pkg;
+    free(pkg_node);
+
+    // Get first station lock
+    struct instrNode* curInstr = my_package->instrListHead;
+
+    enum instr instruction  = curInstr->instruction;
+    get_station_lock(instruction);
+
     while(curInstr != NULL){
       // do work
+
+      // Get the next station lock
+      curInstr = curInstr->next;
+
+      enum instr instruction2  = curInstr->instruction;
+      get_station_lock(instruction2);
+
+      // Get
     }
 
   }
@@ -134,6 +187,8 @@ void fillPkg(struct package* pkg){
     if(head == NULL && tail == NULL){
       struct instrNode* nextNode = malloc(sizeof(struct instrNode));
       nextNode->instruction = instrEnum;
+      nextNode->next = NULL;
+      nextNode->prev = NULL;
 
       head = nextNode;
       tail = nextNode;
@@ -143,21 +198,36 @@ void fillPkg(struct package* pkg){
       nextNode->instruction = instrEnum;
 
       tail->next = nextNode;
+      nextNode->prev = tail;
       tail = nextNode;
     }
 
   }
 
-  struct instrNode* nextNode = malloc(sizeof(struct instrNode));
-  nextNode->instruction = DONE;
-
-  tail->next = nextNode;
-  tail = nextNode;
-
   // Put the instruction list in the package
-  pkg->instrList = head;
+  pkg->instrListHead = head;
+  pkg->instrListTail = tail;
 }
 
+void create_worker(int ids[TEAM_SIZE], enum color type){
+
+  for(int i=0; i<TEAM_SIZE; i++){
+    pthread_t thread;
+    ids[i] = i;
+
+    struct workerArgs* arg = malloc(sizeof(struct workerArgs));
+    arg->id = ids[i];
+    arg->c = type;
+
+    pthread_create(&thread, NULL, worker, (void*)arg);
+  }
+}
+
+/*TODO
+Solution: If the path of a package does not backtrack the path of any other
+currenlty processing package then it is allowed to pick up the first lock for
+its station and start.
+ */
 int main(void){
   FILE* seed_file = fopen("seed.txt", "r");
   int seed = -1;
@@ -205,27 +275,40 @@ int main(void){
     }
   }
 
-  // // Print out the package queue
-  // struct pkgQueueNode* curNode = head;
-  // for(int i=0; i<NUM_PACKAGES; i++){
+  // // Create the worker threads
+  // int red_ids[TEAM_SIZE];
+  // create_worker(red_ids, RED);
   //
+  // int blue_ids[TEAM_SIZE];
+  // create_worker(blue_ids, BLUE);
   //
-  //   printf("Pkg ID: %d, Instr: ", curNode->pkg->id);
-  //   struct instrNode* curInstr = curNode->pkg->instrList;
-  //   while(1){
-  //     printf("%d ", curInstr->instruction);
+  // int green_ids[TEAM_SIZE];
+  // create_worker(green_ids, GREEN);
   //
-  //     if(curInstr->next == NULL){
-  //       break;
-  //     } else{
-  //       curInstr = curInstr->next;
-  //     }
-  //   }
-  //
-  //   curNode = curNode->next;
-  //
-  //   printf("\n");
-  // }
+  // int yellow_ids[TEAM_SIZE];
+  // create_worker(yellow_ids, YELLOW);
+
+  // Print out the package queue
+  struct pkgQueueNode* curNode = head;
+  for(int i=0; i<NUM_PACKAGES; i++){
+
+
+    printf("Pkg ID: %d, Instr: ", curNode->pkg->id);
+    struct instrNode* curInstr = curNode->pkg->instrListHead;
+    while(1){
+      printf("%d ", curInstr->instruction);
+
+      if(curInstr->next == NULL){
+        break;
+      } else{
+        curInstr = curInstr->next;
+      }
+    }
+
+    curNode = curNode->next;
+
+    printf("\n");
+  }
 
   return 0;
 }
